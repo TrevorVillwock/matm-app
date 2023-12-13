b738978aae71cb6063892281e6e3033f7efa1bdf
Trevor Villwock <trevorvillwock@gmail.com>
2023-12-13 10:39:07 -0600

    creates slider for number of synth voices

---
+++ original-projects/soundscape1/soundscape1.py
@@ -0,0 +1,122 @@
+from pyo import *
+from random import uniform
+import wx
+import json
+
+s = Server().boot()
+
+# This tells pyo to place the recording in the same directory as the file and name it test.wav
+"""
+
+with open("./soundscape1.json") as j:
+    app_data = json.loads(j.read())
+    app_data["take"] += 1
+   
+take_number = app_data["take"]
+"""
+
+s.recordOptions(filename = "./soudscape1.wav")
+s.recstart()
+
+"""
+harmonic_series = [100, 200, 300, 400, 500, 600, 700, 800]
+whole_tone = Choice(midiToHz([60, 62, 64, 66, 68, 70, 72]))
+major = Choice(midiToHz([60, 62, 64, 65, 67, 69, 71, 72]))
+natural_minor = Choice(midiToHz([60, 62, 63, 65, 67, 68, 70, 72]))
+major_pent = Choice(midiToHz([60, 62, 64, 67, 69, 72]))
+minor_pent = Choice(midiToHz([60, 63, 65, 67, 70, 72]))
+"""
+# note choices
+harmonic_series = [100, 200, 300, 400, 500, 600, 700, 800]
+whole_tone = midiToHz([60, 62, 64, 66, 68, 70, 72])
+major = midiToHz([60, 62, 64, 65, 67, 69, 71, 72])
+natural_minor = midiToHz([60, 62, 63, 65, 67, 68, 70, 72])
+major_pent = midiToHz([60, 62, 64, 67, 69, 72])
+minor_pent = midiToHz([60, 63, 65, 67, 70, 72])
+
+class StepSeq:
+    def __init__(self, scale):
+        self.filter_lfo = Sine(random.uniform(0.5, 20)).range(500, 2000)
+        self.speed_lfo = Sine(random.uniform(0.5, 20)).range(1, 20)
+        self.notes = scale
+        self.random_note = Choice(self.notes, self.speed_lfo)
+        self.synth = SuperSaw([self.random_note, self.random_note])
+        self.filter = MoogLP(self.synth, self.filter_lfo)
+        # self.filter.ctrl()
+
+class SeqManager:
+    def __init__(self):
+        self.step_sequencers = []
+        self.active_voices = 0
+    
+    def set_num_voices(self, num):
+        i = self.active_voices - 1
+        if num < self.active_voices - 1:  
+            # print("if") 
+            while i >= num:
+                synth_mixer.setAmp(i, 0, 0)
+                i -= 1
+                self.active_voices -= 1    
+        elif num > self.active_voices and num < 10:
+            # print("elif")
+            while i <= 9:
+                # print("while")
+                synth_mixer.setAmp(i, 0, 0.07)
+                self.active_voices += 1
+                i += 1
+       
+sm = SeqManager()
+
+synth_mixer = Mixer(outs=3, chnls=2, time=0.5).out()
+
+audio_file = SfPlayer("./bamboo_chimes.wav", speed=-0.5, loop=True, mul=0.3)
+audio_file.ctrl()
+
+sfdelay = Delay(audio_file, delay=[1, 2], feedback=.5).out()
+sfdelay.ctrl()
+
+for i in range(0, 10):
+    new_step_seq = StepSeq(natural_minor)
+    sm.step_sequencers.append(new_step_seq)
+    synth_mixer.addInput(i, new_step_seq.filter)
+    synth_mixer.setAmp(i, 0, 0.07)
+    sm.active_voices += 1
+
+def event_0():
+    print("event_0")
+    
+    
+def event_1():
+    print("event_1")
+    sm.set_num_voices(1)
+    
+def event_2():
+    print("event_2")
+    sm.set_num_voices(3)
+    
+def event_3():
+    print("event_3")
+    audio_file.speed = 1.0
+    
+def num_synths_slider_move(e):
+    sm.set_num_voices(e.GetEventObject().GetValue())
+
+metro = Metro(4).play()
+count = Counter(metro, min=0, max=8)
+score = Score(count, fname="event_")
+
+# s.gui(locals())
+
+app = wx.App(False)
+control_window = wx.Frame(None, wx.ID_ANY, "Hello")
+control_window.Show(True)
+num_synths_slider = wx.Slider(control_window, 0, 10, pos=wx.Point(0, 20), minValue=1, maxValue=10)
+num_synths_slider.Bind(wx.EVT_SLIDER, num_synths_slider_move)
+
+
+# voices_slider4 = wx.Slider(control_window, 0, 10)
+s.start()
+app.MainLoop()
+
+s.recstop()
+#s.gui(locals())
