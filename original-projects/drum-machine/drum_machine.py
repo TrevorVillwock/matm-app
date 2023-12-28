2c7dbc9f8543e073415242fbd91d9a44e3307632
Trevor Villwock <trevorvillwock@gmail.com>
2023-12-28 12:24:26 -0600

    committing before adding classes

--- original-projects/drum-machine/drum_machine.py
+++ original-projects/drum-machine/drum_machine.py
@@ -1,13 +1,25 @@
+# TODO:
+# Sync tempo changes
+# Create sliders for kick and snare
+# Add effects
+# Create number boxes to display tuplets
+# switch to tkinter?
+# resolve dependency issues
+
 # from pyo import Metro, Counter, Score, TrigFunc
 import pyo
 import wx
+import numpy as np
+import sys
 
 s = pyo.Server().boot()
 s.start()
 
 num_beats = 4
 subdivision = 4
-tempo = pyo.Sig(0.25) # in milliseconds
+tempo = 0.25
+# tempo = pyo.Sig(0.25) # in milliseconds
+# tempo.ctrl()
 
 # samples
 kick_sample = pyo.SfPlayer("./samples/kick/FL_LOFI_Kit09_Kick.wav")
@@ -20,6 +32,10 @@ snare_click = pyo.Metro(tempo).play()
 snare_click.ctrl()
 kick_click = pyo.Metro(tempo).play()
 kick_click.ctrl()
+main_click = pyo.Metro(tempo).play()
+main_click.ctrl()
+
+hihat_tempo = 0.25
 
 # Create a signal to represent the count that we can pass to other places 
 hihat_counter = pyo.Sig(pyo.Counter(hihat_click, min=1, max=num_beats * subdivision + 1))
@@ -30,19 +46,10 @@ def seconds_to_bpm(time):
     bpm = time * 60
     return bpm
 
-# def create_tuplets(click, subdivision):
-#     click.setTime(tempo * 4/subdivision)
-#     print("creating tuplets")
-    
-# create_tuplets(hihat_click, 5)
-
-# hihat_click.setTime(tempo * 4/5)
-
 def set_hihat_tuplet(e):
-    # e.GetEventObject().GetValue()
-    hihat_click.setTime(1.0 / e.GetEventObject().GetValue())
-    
-# et_hihat_tuplet(7)
+    hihat_tempo = 1.0 / e.GetEventObject().GetValue()
+    print(hihat_tempo)
+    # hihat_click.setTime(1.0 / e.GetEventObject().GetValue())
 
 # TODO: wrap this in Events or something similar to use a list to populate match statements
 def play_hihat():
@@ -54,123 +61,43 @@ def play_hihat():
               0, 1, 1, 1, 
               0, 1, 0, 1]
     
+    rhythm = np.repeat(1, 16)
+    
     if rhythm[count - 1] == 1:
         hihat_sample.out()
 
 def play_snare():
-    count = snare_counter.get()
+    count = int(snare_counter.get())
     snare_sample.mul = 0.75 + pyo.RandInt(100) / 100 * 0.25
     
-    match count:
-        case (1):
-            # print(f"count {int(count)}")
-            pass
-        case (2):
-            pass
-            # print(f"count {int(count)}")
-        case (3):
-            pass
-            # print(f"count {int(count)}")
-        case (4):
-            pass
-            # print(f"count {int(count)}")
-        case (5):
-            snare_sample.out()
-            pass
-            # print(f"count {int(count)}")
-        case (6):
-            pass
-            # print(f"count {int(count)}")
-        case (7):
-            pass
-            # print(f"count {int(count)}")
-        case (8):
-            pass
-            # print(f"count {int(count)}")
-        case (9):
-            pass
-            # print(f"count {int(count)}")
-        case (10):
-            pass
-            # print(f"count {int(count)}")
-        case (11):
-            pass
-            # print(f"count {int(count)}")
-        case (12):
-            pass
-            # print(f"count {int(count)}")
-        case (13):
-            snare_sample.out()
-            pass
-            # print(f"count {int(count)}")
-        case (14):
-            pass
-            # print(f"count {int(count)}")
-        case (15):
-            pass
-            # print(f"count {int(count)}")
-        case (16):
-            pass
-            # print(f"count {int(count)}")
+    rhythm = [0, 0, 0, 0,
+              1, 0, 0, 0,
+              0, 0, 0, 0, 
+              1, 0, 0, 0]
+    
+    if rhythm[count - 1] == 1:
+        snare_sample.out()
             
 def play_kick():
-    count = kick_counter.get()
+    count = int(kick_counter.get())
     snare_sample.mul = 0.75 + pyo.RandInt(100) / 100 * 0.25
     
-    match count:
-        case (1):
-            # print(f"count {int(count)}")
-            kick_sample.out()
-        case (2):
-            pass
-            # print(f"count {int(count)}")
-        case (3):
-            pass
-            # print(f"count {int(count)}")
-        case (4):
-            pass
-            # print(f"count {int(count)}")
-        case (5):
-            pass
-            # print(f"count {int(count)}")
-        case (6):
-            pass
-            # print(f"count {int(count)}")
-        case (7):
-            pass
-            # print(f"count {int(count)}")
-        case (8):
-            pass
-            # print(f"count {int(count)}")
-        case (9):
-            kick_sample.out()
-            pass
-            # print(f"count {int(count)}")
-        case (10):
-            pass
-            # print(f"count {int(count)}")
-        case (11):
-            pass
-            # print(f"count {int(count)}")
-        case (12):
-            pass
-            # print(f"count {int(count)}")
-        case (13):
-            pass
-            # print(f"count {int(count)}")
-        case (14):
-            pass
-            # print(f"count {int(count)}")
-        case (15):
-            pass
-            # print(f"count {int(count)}")
-        case (16):
-            pass
-            # print(f"count {int(count)}")
+    rhythm = [1, 0, 0, 0,
+              0, 0, 0, 0,
+              0, 1, 0, 0, 
+              0, 0, 0, 0]
+    
+    if rhythm[count - 1] == 1:
+        kick_sample.out()
+        
+def play_main():
+    hihat_click.setTime(hihat_tempo)
+    print(hihat_tempo)
     
 hihat = pyo.TrigFunc(hihat_click, play_hihat)
 snare = pyo.TrigFunc(snare_click, play_snare)
 kick = pyo.TrigFunc(kick_click, play_kick)
+main = pyo.TrigFunc(main_click, play_main)
 
 app = wx.App(False)
 
@@ -180,6 +107,8 @@ control_window.Show(True)
 hihat_tuplet_slider = wx.Slider(control_window, pos=wx.Point(0, 30), minValue=2, maxValue=10)
 hihat_tuplet_slider.Bind(wx.EVT_SLIDER, set_hihat_tuplet)
 hihat_tuplet_slider_label = wx.StaticText(control_window, label="hihat tuplet", pos=(10, 10))
+
+print(sys.__dict__.keys())
 
 app.MainLoop()
 
